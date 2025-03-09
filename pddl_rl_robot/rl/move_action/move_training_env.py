8422219b9f4ba17ac63d882ba26de760da7058a0
edwardxiaoyicong <yxiaoaz@connect.ust.hk>
2025-03-09 22:18:57 +0800

    playground

---
+++ pddl_rl_robot/rl/move_action/move_training_env.py
@@ -0,0 +1,58 @@
+from robosuite.wrappers import GymWrapper
+import os
+from pddl_rl_robot.simulation.two_peg_one_disk_env import TwoPegOneRoundNut
+from robosuite.wrappers import GymWrapper
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.callbacks import CheckpointCallback
+from datetime import datetime
+
+# NOTE: this class is just a placeholder, please modify the reward based on some observations that are useful for reaching.
+class MoveTrainingEnv(TwoPegOneRoundNut):
+    """
+    Modified version of task - place one round nut with two pegs in the environment.
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    # Override the reward function (please design it such that it favors reaching the goal)
+    def reward(self, action=None):
+        return 10.0
+
+if __name__ == "__main__":
+    # Create environment instance
+    env = MoveTrainingEnv(
+        robots="Panda",  # Use Panda robot
+        gripper_types="default",
+        has_renderer=True,  # Enable visualization
+        has_offscreen_renderer=False,  # Disable offscreen rendering
+        use_camera_obs=False,  # Don't use camera observations
+        reward_shaping=False,  # Enable reward shaping
+    )
+    env = GymWrapper(env)
+
+    # Configurations
+    total_timesteps = 1000
+    save_freq = 100
+    save_path = os.path.dirname(os.path.abspath(__file__))
+    name_prefix = "ppo_panda_reach"
+
+    # Initialize the checkpoint callback
+    checkpoint_callback = CheckpointCallback(
+        save_freq=save_freq,
+        save_path=save_path,
+        name_prefix=name_prefix,
+        save_replay_buffer=True,
+    )
+
+    # Define the model
+    model = PPO("MlpPolicy", env, verbose=1)
+
+    # Train the model
+    model.learn(total_timesteps=total_timesteps, callback=checkpoint_callback)
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model.save(f"{os.path.dirname(os.path.abspath(__file__))}/{name_prefix}_{current_time}_{str(total_timesteps)}")
+    env.close()
+
+    exit()
